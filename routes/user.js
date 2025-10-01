8b801a0b9adc110086c2e9191756162a105205b0
dan-tao <dan.surfer@aol.com>
2025-09-30 20:16:20 -0500

    Rate limit fix 2

--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,24 @@ const router = express.Router();
 const userController = require("../controllers/users");
 const rateLimit = require("express-rate-limit");
 
-// Limitar el número de solicitudes a la ruta de login  para prevenir ataques de fuerza bruta
+// Rate limiter for login
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 10, // Limitar a 10 solicitudes
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
   message:
     "Demasiados intentos de inicio de sesión. Por favor, inténtelo de nuevo más tarde.",
+});
+
+// Rate limiter for register (optional)
+const registerLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 5,
+  message: "Demasiadas solicitudes de registro. Inténtelo más tarde.",
 });
 
 // Rutas
 router.get("/prueba-usuario", userController.pruebaUser);
 router.post("/register", registerLimiter, userController.register);
-router.post("/login", userController.login);
+router.post("/login", loginLimiter, userController.login);
 
 module.exports = router;
